95d96280bfd4dc3b116088936c628dd595ee8de4
Timur Tairov <tairovtk@gmail.com>
2024-07-21 08:44:45 +0300

    commit 1.7 fixed bugs

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -5,7 +5,7 @@ const NavLinks = () => {
     <>
       <Link href='/' className="header-link">Новости</Link>
       <Link href='/blog' className="header-link">Блог</Link>
-      <Link href='/gallary' className="header-link">Галлерея</Link>
+      <Link href='/gallary' className="header-link">Галерея</Link>
       <Link href='/aboutus' className="header-link">О нас</Link>
       <Link href='/contacts' className="header-link">Контакты</Link>
       <Link href='/donate' className="header-link">Пожертвовать</Link>
