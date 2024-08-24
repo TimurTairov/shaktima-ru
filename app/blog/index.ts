ae99ef837652c280de824f4a310652920d6c2270
Timur Tairov <tairovtk@gmail.com>
2024-08-24 17:04:26 +0300

    commit 1.10 fixed bugs

--- app/blog/index.ts
+++ app/blog/index.ts
@@ -3,26 +3,27 @@ export const allArticles = [
     id: '004',
     title: 'День Индии в Москве. Как это было',
     description:
-      'Наша сангха была представлена на дне Индии институтом Риши васиштхи.',
+      'Наша сангха была представлена на дне Индии институтом Риши Васиштхи.',
     img: 'https://sun9-10.userapi.com/impg/1wtOTZeulwobnCsMXCX5rtcvCeEPweAKpSGOmg/kU4In4TOuso.jpg?size=1280x960&quality=95&sign=b9cc32baeb8d31fc87d9e04a39691df9&type=album',
     url: '/blog/indiaday2024',
   },
   {
     id: '003',
-    title: 'Как не попасть в ловушку поверхностной Адвайты',
-    description:
-      'О двенадцати видах поверхностного отношения, подстерегающих практиков недвойственных учений.',
-    img: 'https://sun9-13.userapi.com/impg/O0-i9-XdtpY303sx0EuDOgomhtjTZ0HWZBDE8w/Er-R9kfO9YY.jpg?size=1024x1024&quality=95&sign=35122020907f970364dd5ee47ca300d6&type=album',
-    url: '/blog/neoadwayta',
-  },
-  {
-    id: '002',
     title: 'Махашиваратри 2024',
     description:
       'Мини отчет с прошедшего МахаШиваРатри. + Бонус: видео с мероприятия.',
     img: 'https://sun9-48.userapi.com/impg/7h59rBRfRj_qg5rOo0IQOwwyQWO4GAfRoJMeng/5FzlZOgnIpQ.jpg?size=1024x1024&quality=95&sign=1bd894a23ca9e8d1de2ead52c5ac4c6e&type=album',
     url: '/blog/mahashivaratri2024',
   },
+  {
+    id: '002',
+    title: 'Как не попасть в ловушку поверхностной Адвайты',
+    description:
+      'О двенадцати видах поверхностного отношения, подстерегающих практиков недвойственных учений.',
+    img: 'https://sun9-13.userapi.com/impg/O0-i9-XdtpY303sx0EuDOgomhtjTZ0HWZBDE8w/Er-R9kfO9YY.jpg?size=1024x1024&quality=95&sign=35122020907f970364dd5ee47ca300d6&type=album',
+    url: '/blog/neoadwayta',
+  },
+
   {
     id: '001',
     title: 'Календарь событий 2024',
--- app/gallary/indiaday2024/page.tsx
+++ app/gallary/indiaday2024/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
       <div className="mx-2">
         <h1 className="text-2xl text-center">Дни Индии в Москве с 15 по 18 августа 2024 года.</h1>
         <div className="my-4">
-          <h5 className="font-semibold text-lg text-center text-gray-700">Часть 1. Оьщие фотографии</h5>
+          <h5 className="font-semibold text-lg text-center text-gray-700">Часть 1. Общие фотографии</h5>
           <div className="flex justify-center items-center w-full">
             <ImageGallery items={indiaday__2024} {...settings} />
           </div>
